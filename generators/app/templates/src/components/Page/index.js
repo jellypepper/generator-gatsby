6959cb276b55112c0a5cff3c02a4f19851a7e54a
Sean King <sean@seanking.org>
2019-01-30 15:22:54 +1300

    Minor template updates

--- generators/app/templates/src/components/Page/index.js
+++ generators/app/templates/src/components/Page/index.js
@@ -6,43 +6,45 @@ import favicon from '../../assets/img/favicon.png';
 /**
  * Head component
  * Sets head meta data
- * @property { } title * @property { } description * @property { } thumbnail * @property { } &#x27;&#x27;
+ * @property {string} title Title of the page
+ * @property {string} description Description of the page
+ * @property {string} cover URL to cover image for the page
  */
 export default class Head extends Component {
   static defaultProps = {
     title: '<%= props.name %>',
     description: '<%= props.description %>',
-    withFooter: true
+    cover: ''
   };
 
   render() {
-    const { title, description, withFooter, children } = this.props;
+    const { title, description, children } = this.props;
 
     return (
       <StaticQuery
         render={data => (
           <>
-            {/* Page-specific meta */}
             <Helmet>
               {/* Basic */}
               <title>{title}</title>
               <meta name="description" content={description} />
-              <meta name="author" content="<%= props.name %>" />
+              <meta name="author" content="builtbytomorrow.com" />
               <meta name="rating" content="general" />
-              <link rel="shortcut-icon" href={favicon} />
+              <meta name="image" content={cover} />
 
               {/* Schema.org */}
               <meta itemprop="title" content={title} />
               <meta itemprop="description" content={description} />
+              <meta itemprop="image" content={cover} />
 
-              {/* Social & OpenGraph */}
+              {/* Social */}
               <meta property="og:title" content={title} />
               <meta property="og:description" content={description} />
-
-              {/* Social & OpenGraph */}
+              <meta property="og:image" content={cover} />
               <meta property="twitter:card" content="summary" />
               <meta property="twitter:title" content={title} />
               <meta property="twitter:description" content={description} />
+              <meta property="twitter:image" content={cover} />
             </Helmet>
             {children}
           </>
